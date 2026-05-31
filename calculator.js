6099fbbf42a1159d7ea306c9910cb8a758912353
salonij2211-beep <salonij2211@gmail.com>
2026-05-31 17:18:51 +0530

    Added Task 2 - Calculator Project

---
+++ calculator.js
@@ -0,0 +1,58 @@
+const display =
+document.getElementById("display");
+
+function appendValue(value)
+{
+display.value += value;
+}
+
+function clearDisplay()
+{
+display.value = "";
+}
+
+function deleteLast()
+{
+display.value =
+display.value.slice(0,-1);
+}
+
+function calculate()
+{
+try{
+display.value =
+eval(display.value);
+}
+catch{
+display.value = "Error";
+}
+}
+
+document.addEventListener(
+"keydown",
+function(e)
+{
+const key = e.key;
+
+if(
+"0123456789+-*/.%".includes(key)
+)
+{
+appendValue(key);
+}
+
+if(key==="Enter")
+{
+calculate();
+}
+
+if(key==="Backspace")
+{
+deleteLast();
+}
+
+if(key==="Escape")
+{
+clearDisplay();
+}
+});
